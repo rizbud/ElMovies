5b536a6d5d4e017f43470bd602712a7ede2b2dd4
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2022-01-15 21:31:34 +0700

    feat: add splash screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {ActivityIndicator, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import SplashScreen from '@Containers/SplashScreen';
 import MoviesScreen from '@Containers/Movies';
 import TVScreen from '@Containers/TV';
 import DetailScreen from '@Containers/Detail';
@@ -23,7 +25,6 @@ const TabNavigation = () => {
         activeTintColor: 'white',
         pressColor: apply('gray-300'),
         style: apply('bg-dark'),
-        indicatorStyle: apply('bg-white'),
         tabStyle: apply('row justify-center self-center'),
         iconStyle: apply('pt-1'),
         showIcon: true,
@@ -72,7 +73,12 @@ const AppNavigation = () => {
             },
           }),
         }}
-        initialRouteName="Tab">
+        initialRouteName="SplashScreen">
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="SplashScreen"
+          component={SplashScreen}
+        />
         <Stack.Screen
           options={{headerShown: false}}
           name="Tab"
